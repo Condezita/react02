163e6485888703fdd8a80b5156d872cb13f4852e
nicole conde <condemamani18@gmail.com>
2023-10-19 09:55:08 -0500

    v3. Instalo y conf App .AppForm. boostswash, mat. icons, toastify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+
+import AppForm from "./components/AppForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nicole <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background:"yellow", 
+    width:"350px", 
+    padding:"10px"}}>
+      <AppForm/>
+      <i class="large material-icons">insert_chart</i>
     </div>
   );
 }
